5112f1cf3963fe8a79ea7908824df25411df2566
Victoria Pham <victoriapham52@gmail.com>
2023-04-04 09:44:50 -0700

    FizzBuzz commit

---
+++ FizzBuzz/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace FizzBuzz;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Please input number");
+        var number = int.Parse(Console.ReadLine());
+
+        Console.WriteLine(FizzBuzz(number));
+    }
+
+    public static string FizzBuzz(int userInput)
+    {
+
+        if (userInput % 3 == 0)
+        {
+            return "fizz";
+        }
+
+        if (userInput % 5 == 0)
+        {
+            return "buzz";
+        }
+
+        if (userInput % 3 == 0 && userInput % 5 == 0)
+        {
+            return "fizbuzz";
+        }
+
+        else
+        {
+            return "No fizzbuzz for you :(";
+        }
+    }
+}
+
